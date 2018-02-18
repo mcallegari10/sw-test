57eb75817eb1403432d9050d7ba55d0742e66a45
mcallegari10 <martin.callegari@wolox.com.ar>
2018-02-18 20:34:54 -0300

    change logging

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', { scope: '/sw-example' }).then(reg => {
-    console.log(`Registration complete! ${reg.scope}`)
+  navigator.serviceWorker.register('/sw.js', { scope: '/sw-test' }).then(reg => {
+    if (reg.installing) {
+      console.log('Service worker installing')
+    } else if (reg.active) {
+      console.log('Service worker active')
+    }
   }).catch(err => {
     console.log(`Registration failed! ${err}`)
   })
